35038648dac123851fc0825d4f5bfbbc28e0de7f
Julia Marco <marcootj@gmail.com>
2019-01-02 14:19:41 -0700

    Adds iteration 3

--- lib/curator.rb
+++ lib/curator.rb
@@ -31,4 +31,41 @@ class Curator
     end
   end
 
+  def find_photographs_by_artist(artist)
+    @photographs.find_all do |photo|
+      photo.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    group = @photographs.group_by do |photo|
+      photo.artist_id
+    end
+    artists_id = []
+    group.each do |k,v|
+      if v.count > 1
+        artists_id << v.first.artist_id
+      end
+    end
+    artists_id.map do |artist_id|
+      find_artist_by_id(artist_id)
+    end
+  end
+
+  def photographs_taken_by_artist_from(country)
+    artists = @artists.find_all do |artist|
+      artist.country == country
+    end
+    ids = artists.map do |artist|
+      artist.id
+    end
+    @photographs.find_all do |photo|
+      ids.include?(photo.artist_id)
+    end
+  end
+
+
+
+
+
 end
